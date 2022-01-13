b3cd6322a15b9adda737509974313fc0529e8801
Prateek Jain <prateek.jain@shl.com>
2022-01-13 18:45:33 +0530

    Final Commit for ChatoBot UI

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //Script file
 document.querySelector('.chatbot-icon').addEventListener('click', function(event) {
     document.querySelector('.chatbot-window').classList.toggle('open');
+    document.querySelector('.chatbot-window').classList.toggle('hidden');
 });
