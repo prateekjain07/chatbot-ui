613afa413b6e1402eb21345c35a1ab3667a13b6c
Prateek Jain <prateek.jain@shl.com>
2022-01-14 13:56:31 +0530

    Chatbot UI code

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 document.querySelector('.chatbot-icon').addEventListener('click', function(event) {
     document.querySelector('.chatbot-window').classList.toggle('open');
     document.querySelector('.chatbot-window').classList.toggle('hidden');
+    document.querySelector('.label').classList.toggle('hide')    
 });
